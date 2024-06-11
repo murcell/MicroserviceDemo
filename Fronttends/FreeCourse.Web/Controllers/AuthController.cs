9cdcc961995dfed9051c3f4970618efff24f1c38
murcell <murselgurkan@gmail.com>
2024-06-11 16:11:58 +0300

    the action of signin was coded

--- Fronttends/FreeCourse.Web/Controllers/AuthController.cs
+++ Fronttends/FreeCourse.Web/Controllers/AuthController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FreeCourse.Web.Models;
+using FreeCourse.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace FreeCourse.Web.Controllers
 {
     public class AuthController : Controller
     {
+
+        private readonly IdentityService _identiyService;
+
+        public AuthController(IdentityService identiyService)
+        {
+            _identiyService = identiyService;
+        }
+
         public IActionResult SignIn()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInInput signInInput)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var response =await _identiyService.SignIn(signInInput);
+            if (!response.IsSuccessful)
+            {
+                foreach (var error in response.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error);
+                }
+
+                return View();
+            }
+
+            return RedirectToAction(nameof(Index), "Home");
         }
     }
 }
--- Fronttends/FreeCourse.Web/Models/SignInInput.cs
+++ Fronttends/FreeCourse.Web/Models/SignInInput.cs
@@ -4,10 +4,14 @@ namespace FreeCourse.Web.Models
 {
     public class SignInInput
     {
+        [Required]
         [Display(Name ="Email Adresiniz")]
         public string Email { get; set; }
+
+        [Required]
         [Display(Name = "Şireniz")]
         public string Password { get; set; }
+
         [Display(Name = "Beni Hatırla")]
         public bool IsRemember { get; set; }
     }
