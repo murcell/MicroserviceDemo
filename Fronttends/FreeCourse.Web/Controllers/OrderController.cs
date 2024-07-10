22c5a6d91daa95b8b681cda0019635439bd149c5
murcell <murselgurkan@gmail.com>
2024-07-11 01:39:12 +0300

    order create askenron

--- Fronttends/FreeCourse.Web/Controllers/OrderController.cs
+++ Fronttends/FreeCourse.Web/Controllers/OrderController.cs
@@ -25,22 +25,44 @@ namespace FreeCourse.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Checkout(CheckoutInfoInput checkoutInfoInput)
         {
-            var orderStatus = await _orderService.CreateOrder(checkoutInfoInput);
-            if (!orderStatus.IsSuccessful)
+
+            //// 1. yöntem asenkron ileitişim
+            // var orderStatus = await _orderService.CreateOrder(checkoutInfoInput);
+            //if (!orderStatus.IsSuccessful)
+            //{
+            //    var basket = await _basketService.Get();
+            //    ViewBag.basket = basket;
+            //    ViewBag.error = orderStatus.Error;
+            //    return View();
+            //}
+            // return RedirectToAction(nameof(SuccessfulCheckout), new {orderId=orderStatus.OrderId});
+
+
+            // 2. yol asenktron iletişim. Rabbit MQ'ya gönderiyoruz
+            // Order servis Rabbit MQ'yu dinleyip o şekilde orderi oluşturacak.
+            var orderSuspend = await _orderService.SuspendOrder(checkoutInfoInput);
+            if (!orderSuspend.IsSuccessful)
             {
                 var basket = await _basketService.Get();
                 ViewBag.basket = basket;
-                ViewBag.error = orderStatus.Error;
+                ViewBag.error = orderSuspend.Error;
                 return View();
             }
-
-            return RedirectToAction(nameof(SuccessfulCheckout), new {orderId=orderStatus.OrderId});
+            // burada senkron yöntemde orderId dönüyoruz. asenkronda döndürmeğim için şimdilik 
+            // bu şekilde randon değer ürettim. İleride Payment Id dönülebilir.
+            return RedirectToAction(nameof(SuccessfulCheckout), new { orderId =new Random().Next(1,1000) });
         }
 
         public IActionResult SuccessfulCheckout(int orderId)
         {
             ViewBag.orderId = orderId;
             return View();
+        }
+
+        public async Task<IActionResult> CheckoutHistory()
+        {
+            
+            return View(await _orderService.GetOrder());
         }
 
     }
